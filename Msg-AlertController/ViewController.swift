78587328cf340cab9bd79380ee558e464ffc0441
윤성호 <youn2029@naver.com>
2019-01-21 14:57:03 +0900

    버튼에 트레일링 클로저 추가

--- Msg-AlertController/ViewController.swift
+++ Msg-AlertController/ViewController.swift
@@ -19,16 +19,25 @@ class ViewController: UIViewController {
         let alert = UIAlertController(title: "선택", message: "항목을 선택해주세요", preferredStyle: .alert)
         
         // 취소버튼
-        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        let cancel = UIAlertAction(title: "취소", style: .cancel){
+            (_) in
+            self.result.text = "취소 버튼을 클릭했습니다."
+        }
         
         // 확인버튼
-        let ok = UIAlertAction(title: "확인", style: .default)
+        let ok = UIAlertAction(title: "확인", style: .default){
+            (_) in self.result.text = "확인 버튼을 클릭했습니다."
+        }
         
         // 실행버튼
-        let exec = UIAlertAction(title: "실행", style: .destructive)
+        let exec = UIAlertAction(title: "실행", style: .destructive){
+            (_) in self.result.text = "실행 버튼을 클릭했습니다."
+        }
         
         // 중지버튼
-        let stop = UIAlertAction(title: "중지", style: .default)
+        let stop = UIAlertAction(title: "중지", style: .default){
+            (_) in self.result.text = "중지 버튼을 클릭했습니다."
+        }
         
         // 버튼을 컨트롤러에 등록
         alert.addAction(cancel)
