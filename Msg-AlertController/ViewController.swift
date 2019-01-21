b4c20cbccf7e9483f34aa14ef11bdbc42d17964b
윤성호 <youn2029@naver.com>
2019-01-21 14:46:47 +0900

    알림창 띄우기

--- Msg-AlertController/ViewController.swift
+++ Msg-AlertController/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var result: UILabel!  // 알림창 결과를 표시
+    
+    // 알림창 실행 버튼
+    @IBAction func alert(_ sender: Any) {
+        
+        // 컨트롤러 초기화
+        let alert = UIAlertController(title: "선택", message: "항목을 선택해주세요", preferredStyle: .alert)
+        
+        // 취소버튼
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        
+        // 버튼을 컨트롤러에 등록
+        alert.addAction(cancel)
+        
+        // 알림창 실행
+        self.present(alert, animated: false)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
 
 }
 
